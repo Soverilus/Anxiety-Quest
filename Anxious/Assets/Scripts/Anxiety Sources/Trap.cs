b38689122b139da72237a5cc74e0c3bb019c9d1a
Soverilus <30487703+Soverilus@users.noreply.github.com>
2018-11-08 00:03:25 +0800

    wrote anticlamp

--- Anxious/Assets/Scripts/Anxiety Sources/Trap.cs
+++ Anxious/Assets/Scripts/Anxiety Sources/Trap.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour {
     public float intensity;
+    public float maxX;
     public int damage;
 
     private void OnCollisionEnter2D(Collision2D collision) {
@@ -15,8 +16,23 @@ public class Trap : MonoBehaviour {
                 entityCS.DamageMe(damage);
             }
             targetRB = collision.gameObject.GetComponent<Rigidbody2D>();
-            targetRB.velocity = (new Vector2(targetRB.transform.position.x - collision.GetContact(0).point.x, targetRB.transform.position.y - collision.GetContact(0).point.y).normalized * intensity);
+            Vector2 bounceVector = new Vector2(targetRB.transform.position.x - collision.GetContact(0).point.x, targetRB.transform.position.y - collision.GetContact(0).point.y).normalized;
+            bounceVector.x *= maxX;
+            if (bounceVector.x == 0f) AntiClamp(bounceVector.x, -1f, 1f);
+            if (bounceVector.x > 0f) Mathf.Clamp(bounceVector.x, 1f, maxX);
+            if (bounceVector.x < 0f) Mathf.Clamp(bounceVector.x, -1f, -maxX);
+            if (bounceVector.y != 0f) bounceVector.y /= intensity;
+            targetRB.velocity = bounceVector * intensity;
         }
+    }
 
+    float AntiClamp(float myRandFloat, float minValue, float maxValue) {
+        float calcFloat = myRandFloat;
+        calcFloat += Random.Range(minValue, maxValue);
+        if (calcFloat > -0.01f && calcFloat < 0.01f) {
+            AntiClamp(myRandFloat, minValue, maxValue);
+        }
+        myRandFloat = calcFloat;
+        return myRandFloat;
     }
 }
